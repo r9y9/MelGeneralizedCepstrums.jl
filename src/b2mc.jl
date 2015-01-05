90f7876fbe7916bb9207fdc5af35cd1bf88d9d9a
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-01-05 21:45:17 +0900

    Fix bug in b2mc and add inplace version

--- src/b2mc.jl
+++ src/b2mc.jl
@@ -1,14 +1,21 @@
-function b2mc{T<:FloatingPoint}(b::AbstractVector{T}, α::FloatingPoint)
-    mc = Array(T, length(b))
-    m = length(b)
+function b2mc!{T<:FloatingPoint}(mc::AbstractVector{T}, α::FloatingPoint)
+    m = length(mc)-1
+    o = zero(T)
 
-    mc[m] = b[m]
-
+    mc[m] = mc[m]
+    d = mc[m]
     for i=m-1:-1:1
-        @inbounds mc[i] = b[i] + α*b[i+1]
+        o = mc[i] + α*d
+        d = mc[i]
+        mc[i] = o
     end
 
     mc
+end
+
+function b2mc{T<:FloatingPoint}(b::AbstractVector{T}, α::FloatingPoint)
+    mc = copy(b)
+    b2mc!(mc, α)
 end
 
 # TODO(ryuichi): add b2mc using MelGeneralizedCepstrum type
