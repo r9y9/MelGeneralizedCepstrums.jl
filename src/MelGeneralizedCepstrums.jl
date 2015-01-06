b71569516bd097de26ecfb057580b79216f10f6c
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-01-07 00:49:52 +0900

    Add comments

--- src/MelGeneralizedCepstrums.jl
+++ src/MelGeneralizedCepstrums.jl
@@ -24,26 +24,26 @@ export
     mcep,
 
     # Conversions
-    mc2b,
-    mc2b!,
-    mc2e,
-    mgc2b,
-    mgc2b!,
-    b2mc,
-    b2mc!,
-    b2c,
-    b2c!,
-    gnorm!,
-    gnorm,
-    ignorm!,
-    ignorm,
-    c2ir,
-    freqt,
-    freqt!,
-    frqtr,
-    frqtr!,
-    gc2gc,
-    mgc2mgc
+    mc2b,            # mel-cepstrum -> mlsadf filter coef.
+    mc2b!,           #
+    mc2e,            # mel-cesptrum to energy
+    mgc2b,           # mel-generalized cepstrum -> mglsadf coef.
+    mgc2b!,          #
+    b2mc,            # mlsadf filter coef. -> mel-cepstrum
+    b2mc!,           #
+    b2c,             # b2c in _mgcep.c
+    b2c!,            #
+    gnorm!,          # gain normalization for mel-generalized cepstrum
+    gnorm,           #
+    ignorm!,         # inverse gain normalization for mel-generalized cepstrum
+    ignorm,          #
+    c2ir,            # cepstrum -> impulse response
+    freqt,           # frequency transform
+    freqt!,          #
+    frqtr,           # freqt in _mcep.c
+    frqtr!,          #
+    gc2gc,           # generalized cepstrum -> genralized cepstrum
+    mgc2mgc          # mel-generalized cepstrum -> mel-generalized cepstrum
 
 for fname in [
               "common",
