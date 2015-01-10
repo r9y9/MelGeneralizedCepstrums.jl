7fc3f81615d9bb8df8782cf041fc47e4814185b0
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-01-10 12:48:10 +0900

    small fix on gc2gc

--- src/gc2gc.jl
+++ src/gc2gc.jl
@@ -1,27 +1,23 @@
 function gc2gc{T<:FloatingPoint}(c1::AbstractVector{T}, γ¹::FloatingPoint,
                                  m2::Int, γ²::FloatingPoint)
-    m1 = length(c1)
+    m1 = length(c1)-1
     c2 = zeros(T, m2+1)
     c2[1] = c1[1]
 
     for m=2:m2+1
         ss1, ss2 = 0.0, 0.0
-        min = m1
+        min = (m1 < m-1) ? m1 : m - 2
 
-        if m1 >= m-1
-            min = m-1
-        end
-
-        for k=2:min
+        for k=2:min+1
             @inbounds cc = c1[k] * c2[m-k+1]
             ss2 += (k-1) * cc
             ss1 += (m-k) * cc
         end
 
-        if m <= m1
+        if m <= m1+1
             @inbounds c2[m] = c1[m] + (γ²*ss2 - γ¹*ss1)/(m-1)
         else
-            c2[m] = (γ²*ss2 - γ¹*ss1)/(m-1)
+            @inbounds c2[m] = (γ²*ss2 - γ¹*ss1)/(m-1)
         end
     end
 
