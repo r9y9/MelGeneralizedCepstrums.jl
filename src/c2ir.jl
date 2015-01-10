3629ff7d27cf0e57da62d6a3d341f1e86a5089ca
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-01-10 17:40:22 +0900

    Fix BoundsError in c2ir

--- src/c2ir.jl
+++ src/c2ir.jl
@@ -5,11 +5,8 @@ function c2ir{T<:FloatingPoint}(c::AbstractVector{T}, len::Int)
     h[1] = exp(c[1])
     for n=2:len
         d = 0.0
-        upperlimit = n
-        if n >= m+1
-            upperlimit = m
-        end
-        for k=2:upperlimit+1
+        upperlimit = (n >= m+1) ? m+1 : n
+        for k=2:upperlimit
             @inbounds d += (k-1) * c[k] * h[n-k+1]
         end
         @inbounds h[n] = d / (n-1)
